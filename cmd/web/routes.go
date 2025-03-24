54299f7232c232caac11e24ce67ac20ebf5c207a
Andres <andruix93@gmail.com>
2025-03-24 17:44:12 -0600

    feat: uses middleware to set content headers

---
+++ cmd/web/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() http.Handler {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/createPost", app.snippetCreatePost)
+	mux.HandleFunc("DELETE /snippet/delete/{id}", app.snippetDelete)
+	return commonHeaders(mux)
+}
