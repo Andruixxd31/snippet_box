5294a2ae275e29665df0d0ea30b20a777fdfdefe
Andres <andruix93@gmail.com>
2025-04-09 12:13:34 -0600

    feat: sets logout mechanism

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -254,6 +254,16 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
-func (app *application) userLogout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "user logout")
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've logged out succesfully")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
