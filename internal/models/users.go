a3e29ee6a8a459e641143a02566cd7d904ba3cf8
Andres <andruix93@gmail.com>
2025-04-16 17:52:08 -0600

    feat: implements auth verification through middleware

--- internal/models/users.go
+++ internal/models/users.go
@@ -71,6 +71,10 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 
-func (m *UserModel) Exists(name, email, password string) (bool, error) {
-	return true, nil
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+
+	return exists, err
 }
